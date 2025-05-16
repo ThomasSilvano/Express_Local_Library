b9bd257fdee95faccb78e12fea428d954abf997e
ThomasSilvano <thomas.silvano@isisbem.it>
2025-05-16 08:24:38 +0100

    Updated3

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const dev_db_url =
-  "mongodb+srv://thomassilvano:thomassilvano123@cluster0.me0a833.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://thomassilvano:thomassilvano123@cluster0.me0a833.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
